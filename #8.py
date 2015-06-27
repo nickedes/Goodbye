ef6fc21fd9cc10b0636619b5492436ce809daf06
nickedes <nkmittal4994@gmail.com>
2015-06-28 00:23:57 +0530

    solved problem 8 :heartbeat:

--- #8.py
+++ #8.py
@@ -1,16 +1,19 @@
 import sys
-import textwrap
 inputs = sys.stdin
 t = int(next(inputs))
 for i in range(t):
-    n = int(next(inputs))
-    k = int(next(inputs))
-    num = int(next(inputs))
-    prod = 1
-    textnum = textwrap.wrap(str(num), k)
+    n,k = next(inputs)[:-1].split(' ')
+    n,k = int(n),int(k)
+    num = next(inputs)[:-1]
+    prod = []
+    textnum = []
+    for u in range(len(num)):
+        if len(num[u:u+k]) == k:
+            textnum.append(num[u:u+k])
     for part in textnum:
         tmp = 1
         for j in range(len(part)):
             tmp *= int(part[j])
-        if tmp > prod: prod = tmp
-    print prod
+        prod.append(tmp)
+        
+    print max(prod)
