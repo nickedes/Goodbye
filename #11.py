8eeb68f3c385742c821b279afbfcf380f78539fc
nickedes <nkmittal4994@gmail.com>
2015-06-28 18:50:36 +0530

    solution accepted :collision:
    the previous was not taking all the diagonals

--- #11.py
+++ #11.py
@@ -6,19 +6,13 @@ for i in range(20):
     if i <19:row = next(inputs)[:-1].split(' ')
     else:row = next(inputs).split(' ')
     for j in row: mat[i].append(int(j))
-prod = 0
-for j in range(20):
-    for k in range(16):
-        tmp = mat[j][k]*mat[j][k+1]*mat[j][k+2]*mat[j][k+3]
-        if tmp > prod: prod = tmp
-        tmp = mat[k][j]*mat[k+1][j]*mat[k+2][j]*mat[k+3][j]
-        if tmp > prod: prod = tmp
-for j in range(16):
-    for k in range(16):
-        tmp = mat[j][k]*mat[j+1][k+1]*mat[j+2][k+2]*mat[j+3][k+3]
-        if tmp > prod: prod = tmp
-for j in range(3,20):
-    for k in range(16):
-        tmp = mat[j][k]*mat[j-1][k+1]*mat[j-2][k+2]*mat[j-3][k+3]
-        if tmp > prod: prod = tmp
-print prod
+maxp = 0
+for i in range(20):
+    for j in range(17):
+        up_down = max(mat[i][j] * mat[i][j+1] * mat[i][j+2] * mat[i][j+3],
+                  mat[j][i] * mat[j+1][i] * mat[j+2][i] * mat[j+3][i])
+        if i < 16:
+            diag = max(mat[i][j] * mat[i+1][j+1] * mat[i+2][j+2] * mat[i+3][j+3],
+                     mat[i][j+3] * mat[i+1][j+2] * mat[i+2][j+1] * mat[i+3][j])
+        maxp = max(maxp, up_down, diag)
+print maxp
