03de4722b6d9ab1c86d16de10d6eec91695e25da
nickedes <nkmittal4994@gmail.com>
2015-06-28 12:22:26 +0530

    something better! :tea:

--- #10.py
+++ #10.py
@@ -1,45 +1,29 @@
 import sys
-import time
 from math import sqrt
 from itertools import count, islice
 
+def sumPrime(n):
+    sieve = [True] * n
+    for i in xrange(3,int(n**0.5)+1,2):
+        if sieve[i]:
+            sieve[i*i::2*i]=[False]*((n-i*i-1)/(2*i)+1)
+    return [2] + [i for i in xrange(3,n,2) if sieve[i]]
 
-def isPrime(n):
-    if n < 2: return False
-    for i in islice(count(3,2), int(sqrt(n)-1)//2):
-        if n%i ==0:
-            return False
-    return True
-
-# inputs = sys.stdin
-# t = int(next(inputs))
-start_time = time.time()
-t = 1
+inputs = sys.stdin
+t = int(next(inputs))
 primes = [2]
+prime_million = sumPrime(2000000+1)
 for i in range(t):
-    # n = int(next(inputs))
-    n = 1000
-    num = {}
-    for i in islice(count(2), n-1):
-        num[i] = 0
-    print time.time() - start_time
-    p = 2
-    while True:
-        flag = 0
-        for data in num:
-            if num[data] == 0 and data % p == 0 and data > p:
-                num[data] = 1
-        for key,value in num.items():
-            if key > p and value == 0:
-                flag = 1
-                p = key
+    n = int(next(inputs))
+    if n == 1:
+        print 1
+    elif n < 2000000:
+        sum_p = 0
+        for x in prime_million:
+            if x <= n:
+                sum_p += x
+            else:
                 break
-        if flag == 0:
-            break
-        print time.time() - start_time
-    sum_p = 0
-    for key,value in num.items():
-        if value == 0:
-            sum_p += key
-    print sum_p
-    print time.time() - start_time
+        print sum_p
+    else:
+        print sum(sumPrime(n+1))
