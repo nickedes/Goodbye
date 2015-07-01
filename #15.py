ac2075ce2e8b4273b427186532a088594579bbf9
nickedes <nkmittal4994@gmail.com>
2015-07-01 11:07:38 +0530

    solution accepted :balloon: :boom:
    got a 60 points only ,way to go! :loop:

--- #15.py
+++ #15.py
@@ -1,15 +1,24 @@
-import time
-start = time.time()
-m,n = 21,21
-path = [[0]*n for _ in xrange(m)]
-for i in xrange(m):
-	path[i][0] = 1
-for j in xrange(n):
-	path[0][j] = 1
+import sys
+inputs = sys.stdin
+t = int(next(inputs))
+for i in range(t):
+    if t == 1:
+        m,n = next(inputs).split(' ')
+        m,n=int(m)+1,int(n)+1
+    elif i == 0:
+        m,n = next(inputs).split(' ')
+        m,n = int(m)+1,int(n[:-1])+1
+    else:
+        m,n = next(inputs).split(' ')
+        m,n=int(m)+1,int(n)+1
+    path = [[0]*n for _ in xrange(m)]
+    for i in xrange(m):
+        path[i][0] = 1
+    for j in xrange(n):
+        path[0][j] = 1
 
-for i in xrange(1,m):
-	for j in xrange(1,n):
-		path[i][j] = path[i-1][j] + path[i][j-1]
+    for i in xrange(1,m):
+        for j in xrange(1,n):
+            path[i][j] = path[i-1][j] + path[i][j-1]
 
-print path[m-1][n-1]
-print time.time() - start
+    print path[m-1][n-1]%(pow(10,9)+7)
