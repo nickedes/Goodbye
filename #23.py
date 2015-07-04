c4a50dc64c0eaad39708f6bdca418ba26aaf85d9
nickedes <nkmittal4994@gmail.com>
2015-07-05 00:55:47 +0530

    accepted #23 :telescope: all correct!

--- #23.py
+++ #23.py
@@ -27,33 +27,40 @@ def sum_divisors(num):
         sum_factors *= (pow(prime, prime_fac[prime]+1)-1)/(prime -1)
     return sum_factors-n
 
-start = time()
 abundant = []
 for i in xrange(12,28112):
     if i < sum_divisors(i):
         abundant.append(i)
-
-hav_abundant_sum = []
-for i in xrange(1,28124):
+inputs = sys.stdin
+t = int(next(inputs))
+for x in xrange(t):
+    num = int(next(inputs))
+    is_anundant =  1
+    if num > 28123:
+        print 'YES'
+        is_anundant = 0
+    else:
     # if direct multiple of an abundant!
-    flag = 0
-    for x in abundant:
-        if i%x == 0 and x != i:
-            flag = 1
-            hav_abundant_sum.append(i)
-            break
-    if flag == 0:
-        # check for pair in abundant[] with sum as i
-        left,right = 0,len(abundant)-1
-        while left < right:
-            if abundant[left] + abundant[right] == i:
-                hav_abundant_sum.append(i)
+        i = num
+        flag = 0
+        for x in abundant:
+            if i%x == 0 and x != i:
+                flag = 1
+                print 'YES'
+                is_anundant = 0
                 break
-            elif abundant[left] + abundant[right] < i:
-                left += 1
-            else:
-                right -= 1
-# print hav_abundant_sum[:100]
-total = 28123*(28124)/2
-print total - sum(hav_abundant_sum)
-print time()-start
+        if flag == 0:
+            # check for pair in abundant[] with sum as i
+            left,right = 0,len(abundant)-1
+            while left < right:
+                if abundant[left] + abundant[right] == i:
+                    print 'YES'
+                    is_anundant = 0
+                    break
+                elif abundant[left] + abundant[right] < i:
+                    left += 1
+                else:
+                    right -= 1
+
+    if is_anundant:
+        print 'NO'
