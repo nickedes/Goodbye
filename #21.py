34b11c6f23d8fcc86b3554e8ab214488b6023dae
nickedes <nkmittal4994@gmail.com>
2015-07-04 16:30:18 +0530

    optimised :chart_with_upwards_trend: 21st
    It finds sum of all divisors really fast :horse_racing:

--- #21.py
+++ #21.py
@@ -1,16 +1,38 @@
 import time
+from math import sqrt
 def sum_divisors(num):
-	factors = [1]
-	for i in xrange(2, num/2+1):
-		if num % i == 0:
-			factors.append(i)
-	return sum(factors)
+    """
+    Gives a dict of prime factors of the number num, along with their powers.
+
+    Then calculates the sum of all possible divisors from the prime factors.
+    """
+    n = num
+    prime_fac = {}
+    if num%2 == 0:
+        prime_fac[2] = 0
+    while num%2 == 0:
+        prime_fac[2] += 1
+        num /= 2
+    for i in range(3,int(sqrt(num))+1,2):
+        if num%i == 0:
+            prime_fac[i] = 0
+        while num%i == 0:
+            prime_fac[i] += 1
+            num /= i
+    if num > 2:
+        prime_fac[num] = 1
+    sum_factors = 1
+    for prime in prime_fac:
+        sum_factors *= (pow(prime, prime_fac[prime]+1)-1)/(prime -1)
+    return sum_factors-n
 
 start = time.time()
-am = []
-for i in xrange(200,100000):
-	num = sum_divisors(i)
-	if i == sum_divisors(num) and num != i and i not in am:
-		am.append(i)
-		am.append(num)
-print sum(am),"in",time.time()-start
+am = [0]
+n = 100000
+for i in xrange(2,n):
+    num = sum_divisors(i)
+    if i == sum_divisors(num) and num != i and i not in am:
+        am.append(i)
+        am.append(num)
+print sum(am)
+print time.time()-start
