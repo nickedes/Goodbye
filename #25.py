07fed3df318e3fc61b6f2526820bf5150740f7da
nickedes <nkmittal4994@gmail.com>
2015-07-07 13:00:05 +0530

    accepted 25th, :pineapple: but timeout for 1 case.

--- #25.py
+++ #25.py
@@ -1,15 +1,17 @@
-a,b=1,1
-digit = 1
-index = 1
-n = 1000
-while digit:
-	if len(str(a)) == n:
-		print index
-		digit = 0
-	else:
-		c = a + b
-		a = b
-		b = c
-		index += 1
-
-print 'done!'
+import sys
+inputs = sys.stdin
+t = int(next(inputs))
+for x in xrange(t):
+    n = int(next(inputs))
+    a,b=1,1
+    digit = 1
+    index = 1
+    while digit:
+        if len(str(a)) == n:
+            print index
+            digit = 0
+        else:
+            c = a + b
+            a = b
+            b = c
+            index += 1
