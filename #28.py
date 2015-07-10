f5adf4f33cebf9261d5b26f8d347a20a2c975c25
nickedes <nkmittal4994@gmail.com>
2015-07-10 12:20:38 +0530

    accepted solution :ticket: :tada:

--- #28.py
+++ #28.py
@@ -1,5 +1,8 @@
-sum_diag, d, L = 1, 1, 1001
-for n in range(2, L, 2):
-    sum_diag += 4*d + 10*n
-    d += 4*n;
-print sum_diag
+import sys
+inputs = sys.stdin
+t = int(next(inputs))
+for x in xrange(t):
+    L = int(next(inputs))
+    n = (L-1) // 2
+    sum_d = (16*n**3 + 30*n**2 + 26*n + 3) // 3
+    print sum_d % (10**9+7)
