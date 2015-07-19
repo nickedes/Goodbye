30c92c87fd990e8ab336b509c08ba6f78e31f653
nikhil <nkmittal4994@gmail.com>
2015-07-19 11:19:47 +0530

    Pandigital products,works for all N but not for N=7.

--- #32.py
+++ #32.py
@@ -1,16 +1,27 @@
+import sys
 def check_pandigital(string, s=9):
     string = str(string)
     return len(string) == s and not '1234567890'[:s].strip(string)
 
+inputs=sys.stdin
+n = int(next(inputs))
 pandigital = set()
-for i in range(2, 60):
-    if i < 10:
-        start = 1234
-    else:
-        start = 123
-    for j in range(start, 10000//i):
-        string = str(i)+str(j)+str(i*j)
-        if check_pandigital(string):
-            pandigital.add(i*j)
+if n > 7:
+    for i in range(2, 60):
+        if i < 10:
+            start = 1234
+        else:
+            start = 123
+        for j in range(start, 10000//i):
+            string = str(i)+str(j)+str(i*j)
+            if check_pandigital(string,n):
+                pandigital.add(i*j)
+else:
+    pandigital = set()
+    for i in range(1,10):
+        for j in range(1000):
+            string = str(i)+str(j)+str(i*j)
+            if check_pandigital(string,n):
+                pandigital.add(i*j)
 
 print sum(pandigital)
