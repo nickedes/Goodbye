cac3e03cd965d1fb46b6320b6a53ca6e98a85bf5
nikhil <nkmittal4994@gmail.com>
2015-07-19 16:51:18 +0530

    Digit cancelling in a more better way! :tada:

--- #33.py
+++ #33.py
@@ -1,7 +1,50 @@
-d = 1
-for i in range(1, 10):
-    for j in range(1, i):
-        q, r = divmod(9*j*i, 10*j-i)
-        if not r and q <= 9:
-            d *= i/float(j)
-print d
+# Logic:
+# For each potential fraction (10..99 / 10..99 )
+#     If the num or denom are mod 10, skip it
+#     if the num and denom share a common digit
+#         divide num and denom and store the result in 'expected'
+#         remove the common digit from num and denom (generating num' and denom')
+#         divide num' and denom' and compare the result to 'expected'
+#         if the results match then I have found one of the four answers
+#     next
+# Multiply the four fractions
+# Report the denominator in lowest common terms to the website
+trivials = {}
+d=1
+for i in range(10,100):
+    for j in range(i,100):
+        if i % 10 == 0 or j % 10 == 0:
+            continue
+        if i == j:
+            continue
+        if str(i)[0] == str(j)[0]:
+            if i/float(j) == int(str(i)[-1])/float(int(str(j)[-1])):
+                trivials[i] = j
+                d *= i/float(j)
+                
+        elif str(i)[0] == str(j)[-1]:
+            if i/float(j) == int(str(i)[-1])/float(int(str(j)[0])):
+                trivials[i] = j
+                d *= i/float(j)
+                
+        elif str(i)[-1] == str(j)[0]:
+            if i/float(j) == int(str(i)[0])/float(int(str(j)[-1])):
+                trivials[i] = j
+                d *= i/float(j)
+
+        elif str(i)[-1] == str(j)[-1]:
+            if i/float(j) == int(str(i)[0])/float(int(str(j)[0])):
+                trivials[i] = j
+                d *= i/float(j)
+
+        else:
+            continue
+print trivials,1/d
+# d = 1
+# for i in range(1, 10):
+#     for j in range(1, i):
+#         q, r = divmod(9*j*i, 10*j-i)
+#         if not r and q <= 9:
+#             d *= i/float(j)
+# print d
+
