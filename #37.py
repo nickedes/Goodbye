6730adcc0e6ca442832ff4b91cf6a1d9291be592
nikhil <nkmittal4994@gmail.com>
2015-07-25 10:42:51 +0530

    solving truncatable primes

---
+++ #37.py
@@ -0,0 +1,32 @@
+from math import sqrt
+from itertools import count, islice
+
+
+def isPrime(num_list):
+    for num in num_list:
+        n = int(num)
+        if n < 2 or n % 2 == 0:
+            return False
+        for i in islice(count(3, 2), int(sqrt(n)-1)//2):
+            if n % i == 0:
+                return False
+    return True
+
+
+def all_subs(num):
+    str_num = str(num)
+    all_substrings = set()
+    for i in range(len(str_num)):
+        all_substrings.add(str_num[i:])
+        all_substrings.add(str_num[:i])
+    all_substrings.remove('')
+    return all_substrings
+
+counter, sum_, num = 0, 0, 37
+while counter < 11:
+    if isPrime(all_subs(num)):
+        sum_ += num
+        counter += 1
+        print num
+    num += 2
+print sum_
