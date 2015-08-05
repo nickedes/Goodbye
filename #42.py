eafdfcf252fd1903289356166395b49ec657913d
nikhil <nkmittal4994@gmail.com>
2015-08-05 17:42:58 +0530

    Coded triangle numbers : solved :tada:

--- #42.py
+++ #42.py
@@ -2,7 +2,10 @@ from math import sqrt
 
 
 def wordValue(word):
-    """
+    """Calculate the word value.
+
+    Converting each letter in a word to a number corresponding to its 
+    alphabetical position and adding these values we form a word value.
     """
     val = 0
     for alpha in word[1:len(word)-1]:
@@ -12,16 +15,20 @@ def wordValue(word):
 
 def isTriangular(val):
     """
+    val -- word value
     n -- nth term
     """
     n = (sqrt(1+8*val)-1)/2
-    pass
+    if int(n) == n:
+        return True
+    return False
 
 
 with open('p042_words.txt') as f:
     all_words = f.read().split(",")
+    count = 0
     for word in all_words:
-        # Calculate the value of word!
-        value = wordValue(word)
-
-        pass
+        # Calculate the value of word and if it's triangular!
+        if isTriangular(wordValue(word)):
+            count += 1
+    print count
