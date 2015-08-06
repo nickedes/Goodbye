9f0ed046fa7eefb9075729219d74f429f0b34a36
nikhil <nkmittal4994@gmail.com>
2015-08-06 18:28:25 +0530

    Sub-string divisibility : solved!

--- #43.py
+++ #43.py
@@ -7,16 +7,33 @@ def getPerm():
     pass
 
 
-def substring_divisible():
+def substring_divisible(pandigital):
     """
     Check substring divisiblity as per problem statement here
     https://projecteuler.net/problem=43
     """
-    pass
+    Prime_list = [2, 3, 5, 7, 11, 13, 17]
+    for x in range(1, len(pandigital)-2):
+        if int(pandigital[x:x+3]) % Prime_list[x-1] != 0:
+            return False
+    return True
 
 
 def sumall():
     """
     Calculate sum of all 0-9 pandigital no.s with substring divisiblity.
     """
-    pass
+    # all perms
+    perms = getPerm()
+    count = 0
+    while True:
+        try:
+            # Get next permutation from generator!
+            next_permutation = next(perms)
+            num = ''.join(str(x) for x in next_permutation)
+            if substring_divisible(num):
+                count += int(num)
+        except:
+            return count
+
+print sumall()
