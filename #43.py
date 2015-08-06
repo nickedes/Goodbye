a8be6942394e1b2f46750e6ad7aa7b22d9cb5557
nikhil <nkmittal4994@gmail.com>
2015-08-06 18:02:33 +0530

    Sub-string divisibility : SOLVING

---
+++ #43.py
@@ -0,0 +1,22 @@
+from itertools import permutations
+
+
+def getPerm():
+    """Get all the permutations of 0-9"""
+    return permutations(range(10))
+    pass
+
+
+def substring_divisible():
+    """
+    Check substring divisiblity as per problem statement here
+    https://projecteuler.net/problem=43
+    """
+    pass
+
+
+def sumall():
+    """
+    Calculate sum of all 0-9 pandigital no.s with substring divisiblity.
+    """
+    pass
