9ff93d961012b2ec3df1d67ded86d2842cad7ec0
nikhil <nkmittal4994@gmail.com>
2015-08-07 17:58:26 +0530

    Triangular, pentagonal, and hexagonal : solved

--- #45.py
+++ #45.py
@@ -1,23 +1,32 @@
+from math import sqrt
+
+
 def isPentagonal(num):
-    """
-    """
-    pass
+    """Checks if the given number is a Pentagonal number or not."""
+    n = (1 + sqrt(1 + 24*num))/6
+    if int(n) == n:
+        return True
+    return False
 
 
 def isHexagonal(num):
-    """
-    """
-    pass
+    """Checks if the given number is a Hexagonal number or not."""
+    n = (1 + sqrt(1 + 8*num))/4
+    if int(n) == n:
+        return True
+    return False
 
 
 def findTriangular():
     """
+    Finds the triangular number which is Pentagonal and Hexagonal.
     """
     term = 285
     while True:
         term += 1
         triang = term*(term+1)/2
         if isPentagonal(triang) and isHexagonal(triang):
-            print triang
-            break
-    pass
+            return triang
+
+
+print findTriangular()
