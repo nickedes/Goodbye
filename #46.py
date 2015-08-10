72ff32fb9f7a67bd82905362ab0c7cb1c30968d1
nikhil <nkmittal4994@gmail.com>
2015-08-10 23:10:23 +0530

    Goldbach's other conjecture : finding the false case

---
+++ #46.py
@@ -0,0 +1,13 @@
+def conjecture(num):
+    """
+    Parameter
+    ---------
+    num : int
+    """
+    pass
+
+if __name__ == '__main__':
+    num = 33
+    while conjecture(num):
+        num += 2
+    print num
