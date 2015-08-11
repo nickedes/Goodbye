817be1c8e964f3df64c272b9887fa4a52d64f7e3
nikhil <nkmittal4994@gmail.com>
2015-08-11 21:14:47 +0530

    Goldbach's other conjecture : modules added.

--- #46.py
+++ #46.py
@@ -1,3 +1,27 @@
+from math import sqrt
+from itertools import count, islice
+
+
+def isPrime(n):
+    if n < 2 or n % 2 == 0:
+        return False
+    for i in islice(count(3, 2), int(sqrt(n)-1)//2):
+        if n % i == 0:
+            return False
+    return True
+
+
+def getodd_prime(num):
+    """
+    Returns all odd prime no.s less than num.
+    """
+    num -= 2
+    while num >= 3:
+        if isPrime(num):
+            yield num
+        num -= 2
+
+
 def conjecture(num):
     """
     Parameter
