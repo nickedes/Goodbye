ada8a463370f0777786bc6539d7fa9071636c128
nikhil <nkmittal4994@gmail.com>
2015-08-17 23:21:43 +0530

    basic start, pep8-ify!

---
+++ #50.py
@@ -0,0 +1,20 @@
+def isPrime(num):
+    """
+    """
+    pass
+
+
+def getPrimes(num):
+    """
+    """
+    pass
+
+
+def sum_primes():
+    """
+    """
+    pass
+
+
+if __name__ == '__main__':
+    pass
